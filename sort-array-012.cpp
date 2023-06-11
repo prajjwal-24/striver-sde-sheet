5f4e3bcf2e2bc3da59a851ce1082f36bffb6b832
Prajjwal Sharma <59313731+prajjwal-24@users.noreply.github.com>
2023-06-11 23:35:30 +0530

    Create sort-array-012.cpp

---
+++ sort-array-012.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=0,r=nums.size()-1,mid=0;
+        while(mid<=r){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[l]);
+                l++,mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[r]);
+                r--;
+            }
+        }
+
+        
+
+    }
+};
