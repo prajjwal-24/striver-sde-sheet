1c99bc722fc2e26ba5ab452b4ba3ba33c9ac0e3a
Prajjwal Sharma <59313731+prajjwal-24@users.noreply.github.com>
2023-06-18 16:25:41 +0530

    Create n-meetings-in-a-room.cpp

---
+++ n-meetings-in-a-room.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    int maxMeetings(int start[], int end[], int n)
+    {
+        vector<pair<int,pair<int,int>>>v;
+        
+        for(int i=0;i<n;i++){
+           v.push_back({end[i],{start[i],i}});
+        }
+        
+        sort(v.begin(),v.end());
+        
+        int count=1;
+        int lim=v[0].first;
+        for(int i=1;i<n;i++){
+            if(v[i].second.first>lim){
+                lim=v[i].first;
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        for (int i = 0; i < n; i++) cin >> start[i];
+
+        for (int i = 0; i < n; i++) cin >> end[i];
+
+        Solution ob;
+        int ans = ob.maxMeetings(start, end, n);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
